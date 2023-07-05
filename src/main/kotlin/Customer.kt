d22837a926f9c72b3fe9397ea41f8d9ee0a2dd93
drAgon-235 <d.slawo@gmail.com>
2023-07-05 13:52:55 +0200

    Abschlussarbeit - Cosmetic-Shop-Salon - Push_01 (test)

---
+++ src/main/kotlin/Customer.kt
@@ -0,0 +1,11 @@
+class Customer {
+    val customNr: Int = 0
+    val name: String = ""
+    val adresse: String = ""
+    val email: String = ""
+    val password: String = ""
+    var saldo: Double = 0.0
+
+
+
+}
